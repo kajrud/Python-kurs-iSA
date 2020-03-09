8c48b87c3be19e0b2ca1a1f259df12af81e403f4
Kaja <60346285+kajrud@users.noreply.github.com>
2020-03-09 21:55:11 +0100

    Delete ex_04.py

--- ex_04.py
+++
@@ -1,5 +0,0 @@
-#Napisz program, który poda pierwszą i ostatnią cyfrę podanej liczby
-
-num = input("Podaj liczbę, im dłuższa tym lepsza: ")
-print("Pierwsza cyfra tej liczby to: ", num[0])
-print("Ostatnia cyfra tej liczby to: ", num [-1])
