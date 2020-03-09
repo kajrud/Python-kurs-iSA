59ae1e0e9cb02321897b814721c53a89b8430250
Kaja <60346285+kajrud@users.noreply.github.com>
2020-03-09 21:55:02 +0100

    Delete ex_03.py

--- ex_03.py
+++
@@ -1,5 +0,0 @@
-#Napisz program do obliczania pola powierzchni koła o zadanym promieniu
-
-radius = float (input("Podaj promień koła: "))
-pi = 3.14
-print("Pole powierzchni koła wynosi ", pi * radius ** 2)
