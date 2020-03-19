1444e535aeb31a45aa51237a52bc7dfdc9a021b2
kajrud <kaja.kinga.rudnicka@gmail.com>
2020-03-19 09:45:17 +0100

    rozmieniarka update 19/03

--- Homeworks/Day 04/ex_03.py
+++ Homeworks/Day 04/ex_03.py
@@ -1,12 +1,12 @@
 # Program przyjmuje kwotę w parametrze i wylicza jak rozmienić to na monety: 5, 2, 1, 0.5, 0.2, 0.1 wydając ich jak najmniej.
 
 def coins_change():
-    money = float(input("Podaj kwotę, którą chcesz rozmienić: "))
-    div_list = (5, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01)
-    while money >= 0.01:
+    money = (float(input("Podaj kwotę, którą chcesz rozmienić: "))) * 100
+    div_list = (500, 200, 100, 50, 20, 10, 5, 2, 1)
+    while money >= 1:
         for i in div_list:
             num = int(money / i)
-            print(f"{num} monet {i} zł.")
+            print(f"{num} monet {i / 100} zł.")
             money = money - num * i
 
 while True:
