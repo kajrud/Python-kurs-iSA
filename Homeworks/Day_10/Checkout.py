dac437855afee31f8785ec2cb21d466af5fdc320
kajrud <kaja.kinga.rudnicka@gmail.com>
2020-04-14 10:15:47 +0200

    14/04

--- Homeworks/Day_10/Checkout.py
+++ Homeworks/Day_10/Checkout.py
@@ -1,29 +1,45 @@
 class Cart():
     def __init__(self):
-        pass
+        self.elements = []
+        self.elements_number = 0
+        self.net = 0
+        self.gross = 0
+
+    def dodaj(self, element):
+        self.elements.append(element)
+        self.elements_number += 1
+        self.net += element.net_price
+        self.gross += element.gross_price()
+
+    def __len__(self):
+        return len(self.elements)
+
+    def net_worth(self):
+        return self.net
+
+    def gross_worth(self):
+        return self.gross
 
 class Item():
-    def __init__(self, ):
-        # self.id
-        # self.name
-        # self.price
-        # self.amount
-        # self.created_at
-        # self.last_buy_at
-        # self.rating
-        pass
+    def __init__(self, id, name, price, amount, created_at, last_buy_at, rating):
+        self.id = id
+        self.name = name
+        self.price = price
+        self.amount = amount
+        self.created_at = created_at
+        self.last_buy_at = last_buy_at
+        self.rating = rating
 
 
 class Book(Item):
-    def __init__(self):
-        # self.author
-        # self.number_of_pages
-        # self.vat
-        # self.isbn
-        pass
+    def __init__(self, self, id, name, price, amount, created_at, last_buy_at, rating, author, number_of_pages):
+        super().__init__(id, name, price, amount, created_at, last_buy_at, rating)
+        self.author = author
+        self.number_of_pages = number_of_pages
+        self.vat = 0.05
+
 
 class Ebook(Book):
-    def __init__(self):
-        # self.format
-        # self.vat
-        pass
+    def __init__(self, self, id, name, price, amount, created_at, last_buy_at, rating, author, number_of_pages, format):
+        super().__init__(id, name, price, amount, created_at, last_buy_at, rating, author, number_of_pages)
+        self.format = format
